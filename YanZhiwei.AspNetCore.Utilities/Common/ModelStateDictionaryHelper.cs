d74227e19a94295f538b83d55b7111db2421d49d
言志伟 <Yan.Zhiwei@hotmail.com>
2017-03-17 10:47:05 +0800

    coding

---
+++ YanZhiwei.AspNetCore.Utilities/Common/ModelStateDictionaryHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace YanZhiwei.AspNetCore.Utilities.Common
+{
+    /// <summary>
+    /// ModelStateDictionary 辅助类
+    /// </summary>
+    public static class ModelStateDictionaryHelper
+    {
+        /// <summary>
+        /// 获取ModelState错误信息
+        /// </summary>
+        /// <param name="modelState">ModelStateDictionary</param>
+        /// <returns>错误信息</returns>
+        public static string GetModelStateError(this ModelStateDictionary modelState)
+        {
+            string _modelStateError = string.Empty;
+            foreach (var item in modelState.Values)
+            {
+                if (item.Errors.Count > 0)
+                {
+                    _modelStateError = item.Errors[0].ErrorMessage;
+                    break;
+                }
+            }
+            return _modelStateError;
+        }
+    }
+}
